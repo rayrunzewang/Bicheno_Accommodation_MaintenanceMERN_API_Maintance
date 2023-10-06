6702479414648d45a9810d208e6fb16db3f8c04d
Ray <rayrunzewang@gmail.com>
2023-10-06 21:16:57 +1100

    dont use login function

--- utils/authUtils.js
+++ utils/authUtils.js
@@ -1,13 +1,13 @@
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
-async function generateHash(password) {
-  const saltRounds = 10;
-  const hashedPassword = await bcrypt.hash(password, saltRounds);
-  return hashedPassword;
-}
+// async function generateHash(password) {
+//   const saltRounds = 10;
+//   const hashedPassword = await bcrypt.hash(password, saltRounds);
+//   return hashedPassword;
+// }
 
-async function comparePassword(password, hashedPassword) {
-  return await bcrypt.compare(password, hashedPassword);
-}
+// async function comparePassword(password, hashedPassword) {
+//   return await bcrypt.compare(password, hashedPassword);
+// }
 
-module.exports = { generateHash, comparePassword };
+// module.exports = { generateHash, comparePassword };
