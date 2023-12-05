f2cec68c4efa9ed9c916f9fabe5df5f1152cee1d
Ray <rayrunzewang@gmail.com>
2023-12-05 12:45:10 +1100

    add www.bichenoaccommodation.com.au to allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['https://bichenoaccommodation.com.au', 'https://bicheno.com.au', 'https://bicheno-accommodation.onrender.com', 'http://localhost:3000'];
+const allowedOrigins = ['https://www.bichenoaccommodation.com.au', 'https://bichenoaccommodation.com.au', 'https://bicheno.com.au', 'https://bicheno-accommodation.onrender.com', 'http://localhost:3000'];
 
 module.exports = allowedOrigins
